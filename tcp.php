7e3356dec07217a7f88167dcc2dd44ca3167c1d1
f2m520 <2200480916@qq.com>
2020-05-23 15:36:11 +0800

    打印

--- tcp.php
+++ tcp.php
@@ -5,7 +5,7 @@ $port = 9501;
 $serv = new swoole_server($host, $port);
 
 $serv->on('connect', function($serv, $fd){
-    var_dump(11);die;
+    var_dump(22);die;
     var_dump($serv);
     var_dump($fd);die;
     echo '建立连接';
