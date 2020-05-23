d5fb110e2739d054159e8e03274545b6bee41d55
f2m520 <2200480916@qq.com>
2020-05-23 14:37:12 +0800

    提交文件

---
+++ tcp.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = '192.168.1.200';
+$port = 9501;
+$serv = new swoole_server($host, $port);
+
+$serv->on('connect', function($serv, $fd){
+    var_dump($serv);
+    var_dump($fd);die;
+    echo '建立连接';
+});
