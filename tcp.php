1dfc678d60de4cc2102e8f8a10be49ae0c0ab72b
f2m520 <2200480916@qq.com>
2020-05-23 16:03:00 +0800

    '完整tcp服务'

--- tcp.php
+++ tcp.php
@@ -2,12 +2,37 @@
 
 $host = '192.168.1.200';
 $port = 9501;
-var_dump(11);;die;
+
 $serv = new swoole_server($host, $port);
 
+/**
+ * 连接
+ * $serv 服务端
+ * $fd 客户端
+ */
 $serv->on('connect', function($serv, $fd){
-    var_dump(22);die;
-    var_dump($serv);
-    var_dump($fd);die;
-    echo '建立连接';
+    echo '建立连接\n';
 });
+
+/**
+ * 接收信息
+ * $serv 服务端信息
+ * $fd 客户端信息
+ * $from_id id
+ * $data 数据
+ */
+$serv->on('receive', function($serv, $fd, $from_id, $data){
+    echo '接收信息\n';
+});
+
+/**
+ * 关闭连接
+ */
+$serv->on('close', function($serv, $fd){
+    echo '关闭连接\n';
+});
+
+/**
+ * 启动服务器
+ */
+$serv->start();
