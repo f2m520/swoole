bf8f2a2c79e051d08d5864cd81efa8c73946e409
f2m520 <2200480916@qq.com>
2020-06-04 20:52:43 +0800

    定时器

--- time.php
+++ time.php
@@ -5,6 +5,7 @@ swoole_timer_tick(2000, function($timer_id){
     echo "执行 $timer_id \n";
 });
 
-swoole_timer_after(3000, function($timer_id){
+//单次定时器
+swoole_timer_after(3000, function(){
     echo "3000后执行 \n";
 });
